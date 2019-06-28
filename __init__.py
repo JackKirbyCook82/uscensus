c1649081c9c6fb35c4782170b1c0fb9d65c9456d
JackKirbyCook82 <jackcookchee@yahoo.com>
2019-06-27 22:43:09 -0500

    Updated

--- __init__.py
+++ __init__.py
@@ -7,20 +7,18 @@ Created on Wed Nov 30 2018
 """
 
 import os.path
-import pandas as pd
 import csv
 
 from webdata.webreaders import WebReader
 from webdata.webapi import WebRequestAPI, FTPDownloadAPI
 from utilities.dataframes import dataframe_fromjson
 from variables.geography import Geography
-from variables.date import Date
 
 from uscensus.urlapi import USCensus_URLAPI
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
-__all__ = ['USCensus_WebAPI', 'USCensus_ShapefileAPI']
+__all__ = ['USCensus_WebAPI', 'USCensus_FTPAPI']
 __copyright__ = "Copyright 2018, Jack Kirby Cook"
 __license__ = ""
 
@@ -29,18 +27,13 @@ _DIR = os.path.dirname(os.path.realpath(__file__))
 _DATEFORMATS = {'geoseries':'%Y', 'yearseries':'%Y', 'timeseries':'%Y-%m'}
 _WEBKEYS = ('series', 'survey', 'tags', 'preds', 'concepts')
 
-_GEOTABLEID = 'acs5detail_geo'
-_GEOPARMS = dict(series='geoseries', survey='acs-detail', tags=['NAME'], date=Date(year=2017), estimate=5, preds={})
-_GEODOMAIN = 'ftp2.census.gov'
-_GEOPATH = ['geo','tiger','TIGER{}']
 _GEOFILE = 'geography.csv'
-_GEOFTPFILENAME = 'tl_{year}_{geoid}_{geodir}'
 _GEOAPIKEYS = {}
 _GEODIRNAMES = {}
 
 with open(os.path.join(_DIR, _GEOFILE), mode='r') as infile:
     reader = csv.reader(infile)   
-    for row in reader:        
+    for row in reader:      
         _GEOAPIKEYS[row[0]] = row[1]
         _GEODIRNAMES[row[0]] = row[2]
 
@@ -63,15 +56,12 @@ class USCensus_WebAPI(WebRequestAPI):
     def webkeys(self): return list(_WEBKEYS)
 
     # FILES
-    def filename(self, *args, tablesID, geography, dates, estimate=5, **kwargs):
-        series = list(set([parms['series'] for parms in self.tablequeue.values()]))
-        assert len(series) == 1
-        for date in _aslist(dates): date.setformat(_DATEFORMATS[series[0]])
-        return '_'.join([str(tablesID).format(estimate), geography.geoid, *[str(date) for date in dates]])
+    def filename(self, *args, tableID, geography, date, estimate, **kwargs):
+        return tableID.format(estimate=estimate, date=date, geoid=geography.geoid)
 
     # PROCESSORS
     def webreader(self, *args, tags, **kwargs):
-        url = self.__urlapi(*args, tags=tags, **kwargs)  
+        url = self.__urlapi(*args, tags=['NAME', *_aslist(tags)], **kwargs)  
         return self.__webreader(url)   
 
     def webdataparser(self, webdata, *args, **kwargs):  
@@ -80,6 +70,7 @@ class USCensus_WebAPI(WebRequestAPI):
     def webtableparser(self, webtable, *args, **kwargs):
         webtable = self.__renameheaders(webtable, *args, **kwargs)
         webtable = self.__consolidate(webtable, *args, **kwargs)
+        webtable = self.__splitgeoname(webtable, *args, **kwargs)  
         return self.__combinegeography(webtable, *args, **kwargs)
         
     def webtablescope(self, webtable, *args, geography, estimate, date, **kwargs):
@@ -111,46 +102,34 @@ class USCensus_WebAPI(WebRequestAPI):
         return webtable.drop('NAME', axis=1)
 
     # ENGINES
-    def execute(self, *args, geography, dates, estimate=5, **kwargs):
-        tables = {}
-        for date in _aslist(dates):
+    def generator(self, *args, tableIDs, dates, **kwargs):
+        for date in _aslist(dates): 
             for tableID, parms in self.tablequeue.items():
                 date.setformat(_DATEFORMATS[parms['series']])
-                tables[(str(date), tableID)] = self.download(*args, geography=geography, date=date, estimate=estimate, **parms, **kwargs)
-        return pd.concat(tables.values(), axis=0)
-
-    def geography(self, *args, geography, **kwargs):   
-        try: return self.load(*args, tablesID=_GEOTABLEID, dates=[_GEOPARMS['date']], geography=geography, **kwargs)
-        except FileNotFoundError:
-            webdata = self.webreader(*args, geography=geography, **_GEOPARMS, **kwargs)
-            webtable = self.webdataparser(webdata, *args, **kwargs)
-            webtable = self.__combinegeography(webtable, *args, geography=geography, **kwargs)
-            webtable = self.__splitgeoname(webtable, *args, geography=geography, **kwargs)            
-            self.save(webtable, *args, tablesID=_GEOTABLEID, dates=[_GEOPARMS['date']], geography=geography, **kwargs)
-            return webtable
+                yield {'tableID':tableID, 'date':date, **parms, **kwargs}
+                
+    def execute(self, *args, geography, date, estimate=5, **kwargs):
+        return self.download(*args, geography=geography, date=date, estimate=estimate, **kwargs)
 
 
-class USCensus_ShapefileAPI(FTPDownloadAPI):
-    def __init__(self, *args, year=2018, username='anonymous', password='anonymous', **kwargs): 
-        domain, path = _GEODOMAIN, [item.format(year) for item in _GEOPATH]
-        self.__year = year
-        super().__init__(*args, domain=domain, path=path, filetype='zip', username=username, password=password, **kwargs)
+class USCensus_FTPAPI(FTPDownloadAPI):
+    def __init__(self, *args, username='anonymous', password='anonymous', **kwargs): 
+        super().__init__(*args, domain='ftp2.census.gov', filetype='zip', username=username, password=password, **kwargs)
     
-    def __repr__(self): return '{}(username={}, password={}, year={}, repository={})'.format(self.__class__.__name__, self.username, self.password, str(self.__year), self.repository)
+    def __repr__(self): return '{}(username={}, password={}, repository={})'.format(self.__class__.__name__, self.username, self.password, self.repository)
+
+    def serverpath(self, *args, date, geography, **kwargs): 
+        return '/'.join(['geo','tiger','TIGER{date}'.format(date=date.year), _GEODIRNAMES[geography[-1].getkey(-1)]])
     
-    def pathnames(self, *args, geography, **kwargs):
-        yield _GEODIRNAMES[geography[-1].getkey(-1)]
-    
-    def filenames(self, *args, geography, **kwargs): 
-        geodir = _GEODIRNAMES[geography[-1].getkey(-1)]
-        for i in reversed(range(len(geography))): 
-            geoid = geography[slice(0, i)].geoid
-            yield _GEOFTPFILENAME.format(year=self.__year, geoid=geoid if geoid else 'us', geodir=geodir.lower())
-
-
-
-
-    
+    def serverfilenames(self, *args, geography, date, **kwargs):
+        items = ['tl_{year}_us_{forgeo}'.format(year=date.year, forgeo=_GEODIRNAMES[geography.getkey(-1)].lower())]
+        if 'state' in geography.keys(): items.append('tl_{year}_{state}_{forgeo}'.format(state=geography.getvalue('state'), year=date.year, forgeo=_GEODIRNAMES[geography.getkey(-1)].lower()))
+        return items
+        
+    ###############################################################################################################
+    def clientfilename(self, *args, date, geography, **kwargs): 
+        raise Exception('UNDER CONSTRUCTION')
+    ###############################################################################################################
     
     
     
